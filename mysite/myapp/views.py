a740540b9bb249464e33277786b46eb3933e65d0
GianlorenzoDercole <johndercole01@gmail.com>
2023-02-22 13:40:34 -0800

    added funtionality to edit buttons see views.py line 19 index.html line 54 utls.py line 6 and edit.html line 3

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -12,3 +12,11 @@ def index(request):
     expenses = Expense.objects.all()
     expense_form = ExpenseForm()
     return render(request,'myapp/index.html',{'expense_form':expense_form,'expenses':expenses})
+
+
+
+
+def edit(request,id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+    return render(request,'myapp/edit.html',{'expense_form':expense_form})
