2f17bf8630e835c55c19cd7a09e43178316c3ed3
GianlorenzoDercole <johndercole01@gmail.com>
2023-02-22 13:53:10 -0800

    added functionality to edit view see views.py line 24

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
 # Create your views here.
@@ -17,6 +17,15 @@ def index(request):
 
 
 def edit(request,id):
+    # tell expense form on edit view what data to show
     expense = Expense.objects.get(id=id)
     expense_form = ExpenseForm(instance=expense)
+    # update new data for expense
+    if request.method =='POST':
+        expense = Expense.objects.get(id=id)
+        form = ExpenseForm(request.POST,instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
     return render(request,'myapp/edit.html',{'expense_form':expense_form})
